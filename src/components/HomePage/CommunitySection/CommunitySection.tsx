82df498ac12ca63052665d6feffa02a1c04b019e
PSWai <parksoon_my@hotmail.com>
2023-03-22 06:24:15 +0800

    Update community section

--- src/components/HomePage/CommunitySection/CommunitySection.tsx
+++ src/components/HomePage/CommunitySection/CommunitySection.tsx
@@ -3,38 +3,34 @@ import Translate from "@docusaurus/Translate";
 import Section from "@site/src/components/Section";
 import { RxOpenInNewWindow } from "react-icons/rx";
 
-import bilibiliLogoPath from "./bilibili-logo.png";
-import githubLogoPath from "./github-logo.png";
-import meetupLogoPath from "./meetup-logo.png";
-import slackLogoPath from "./slack-logo.png";
-import twitterLogoPath from "./twitter-logo.png";
-import wechatLogoPath from "./wechat-logo.png";
+import BilibiliLogo from "./bilibili-logo.svg";
+import GithubLogo from "./github-logo.svg";
+import MeetupLogo from "./meetup-logo.svg";
+import SlackLogo from "./slack-logo.svg";
+import TwitterLogo from "./twitter-logo.svg";
+import WechatLogo from "./wechat-logo.svg";
 import wechatQrPath from "./wechat-qr.jpeg";
 
 import styles from "./CommunitySection.module.scss";
 
 type LogoDisplayProps = {
-  imageSrc: string;
+  image: React.ReactNode;
   externalLink?: string;
   hoverImageSrc?: string;
 };
 
 function LogoDisplay(props: LogoDisplayProps) {
-  const { imageSrc, externalLink, hoverImageSrc } = props;
+  const { image, externalLink, hoverImageSrc } = props;
 
   return (
     <a href={externalLink} target="_blank" className={styles.logoDisplayLink}>
       <div className={styles.logoDisplay}>
-        <div className={styles.imageContainer}>
-          <img src={imageSrc} />
-        </div>
-        <div className={styles.hoverImageContainer}>
-          {hoverImageSrc ? (
+        <div className={styles.imageContainer}>{image}</div>
+        {hoverImageSrc && (
+          <div className={styles.hoverImageContainer}>
             <img src={hoverImageSrc} />
-          ) : (
-            <RxOpenInNewWindow size="30%" />
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </a>
   );
@@ -42,27 +38,30 @@ function LogoDisplay(props: LogoDisplayProps) {
 
 function CommunitySection() {
   return (
-    <Section title={<Translate id="homePage.communitySection.title" />}>
+    <div className={styles.container}>
+      <h2 className={styles.header}>
+        <Translate id="homePage.communitySection.title" />
+      </h2>
       <div className={styles.logoGrid}>
         <LogoDisplay
-          imageSrc={githubLogoPath}
+          image={<GithubLogo />}
           externalLink="https://github.com/byconity"
         />
 
-        <LogoDisplay imageSrc={wechatLogoPath} hoverImageSrc={wechatQrPath} />
+        <LogoDisplay image={<WechatLogo />} hoverImageSrc={wechatQrPath} />
 
         <LogoDisplay
-          imageSrc={bilibiliLogoPath}
+          image={<BilibiliLogo />}
           externalLink="https://space.bilibili.com/2065226922?spm_id_from=333.1007.0.0"
         />
 
-        <LogoDisplay imageSrc={twitterLogoPath} />
+        <LogoDisplay image={<TwitterLogo />} />
 
-        <LogoDisplay imageSrc={slackLogoPath} />
+        <LogoDisplay image={<SlackLogo />} />
 
-        <LogoDisplay imageSrc={meetupLogoPath} />
+        <LogoDisplay image={<MeetupLogo />} />
       </div>
-    </Section>
+    </div>
   );
 }
 
